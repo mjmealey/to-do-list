fc8f567004036c20a9d372618c8c20fad546dadf
mjmealey <markjmealey@gmail.com>
2023-10-13 21:21:13 -0500

    function for adding the task through the task block started

--- to-do-list/src/todo/inbox/AddTasks.ts
+++ to-do-list/src/todo/inbox/AddTasks.ts
@@ -1,23 +1,33 @@
-export const AddTasksToInbox = () => {
-    const addTask = document.getElementById("addTask") as HTMLButtonElement
-    const removeElements = (empty: string) => {
-        addTask.style.display = empty 
-        const noTasks = document.getElementById("noTasks") as HTMLParagraphElement
-        noTasks.style.display = empty
-    }
+const AddTasksToInbox = () => {
+    
+  const addTask = document.getElementById("addTask") as HTMLButtonElement;
+  const removeElements = (empty: string) => {
+    addTask.style.display = empty;
+    const noTasks = document.getElementById("noTasks") as HTMLParagraphElement;
+    noTasks.style.display = empty;
+  };
 
-    const displayTaskAdd = (label: string) => {
-       const taskBlock = document.createElement("div")
-       taskBlock.id = "taskBlock" 
-       taskBlock.style.backgroundColor = "green"
-       document.body.appendChild(taskBlock)
-       const nameOfTask = document.createElement(label) as HTMLElement
-       nameOfTask.innerText = "Name"
-       taskBlock.appendChild(nameOfTask)
-    }
+  const displayTaskAdd = (label: string) => {
+    const taskBlock = document.createElement("div");
+    taskBlock.id = "taskBlock";
+    taskBlock.style.backgroundColor = "green";
+    taskBlock.style.width = "50rem"
+    taskBlock.style.padding = "20px"
+    const toDos = document.getElementById("toDos") as HTMLDivElement
+    toDos.appendChild(taskBlock);
+    const nameOfTask = document.createElement(label) as HTMLElement;
+    nameOfTask.innerText = "Name";
+    taskBlock.appendChild(nameOfTask);
+  };
 
+  const addTasks = () => {
+    removeElements("none");
+  };
 
-    addTask.addEventListener("click", () => {
-        removeElements("none")
-    })
-}
+  addTask.addEventListener("click", () => {
+    addTasks();
+    displayTaskAdd("label")
+  });
+};
+
+export default AddTasksToInbox;
