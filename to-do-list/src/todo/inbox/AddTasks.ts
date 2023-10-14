8dc3219155d7032dedbe1b160bb68a4238582f67
mjmealey <markjmealey@gmail.com>
2023-10-13 19:07:16 -0500

    removeElements complete and displayTaskAdd started which will add in the task block where users can enter their to do's

--- to-do-list/src/todo/inbox/AddTasks.ts
+++ to-do-list/src/todo/inbox/AddTasks.ts
@@ -1,6 +1,23 @@
-const AddTasksToInbox = () => {
-    
-    const removeElements = () => {
-        
+export const AddTasksToInbox = () => {
+    const addTask = document.getElementById("addTask") as HTMLButtonElement
+    const removeElements = (empty: string) => {
+        addTask.style.display = empty 
+        const noTasks = document.getElementById("noTasks") as HTMLParagraphElement
+        noTasks.style.display = empty
     }
+
+    const displayTaskAdd = (label: string) => {
+       const taskBlock = document.createElement("div")
+       taskBlock.id = "taskBlock" 
+       taskBlock.style.backgroundColor = "green"
+       document.body.appendChild(taskBlock)
+       const nameOfTask = document.createElement(label) as HTMLElement
+       nameOfTask.innerText = "Name"
+       taskBlock.appendChild(nameOfTask)
+    }
+
+
+    addTask.addEventListener("click", () => {
+        removeElements("none")
+    })
 }
