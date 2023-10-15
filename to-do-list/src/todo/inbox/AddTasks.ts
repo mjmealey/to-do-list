7ec9ea13c9637d4adda3c58f5ebc775a8167d36e
mjmealey <markjmealey@gmail.com>
2023-10-14 21:30:43 -0500

    task description created

--- to-do-list/src/todo/inbox/AddTasks.ts
+++ to-do-list/src/todo/inbox/AddTasks.ts
@@ -6,7 +6,7 @@ const AddTasksToInbox = () => {
     noTasks.style.display = empty;
   };
 
-  const displayTaskAdd = (label: string) => {
+  const displayTaskAdd = () => {
     const taskBlock = document.createElement("div");
     taskBlock.id = "taskBlock";
     taskBlock.style.backgroundColor = "green";
@@ -14,19 +14,49 @@ const AddTasksToInbox = () => {
     taskBlock.style.padding = "20px";
     const toDos = document.getElementById("toDos") as HTMLDivElement;
     toDos.appendChild(taskBlock);
-    const nameLabel = document.createElement(label) as HTMLElement;
-    nameLabel.id = "nameLabel";
-    nameLabel.textContent = "Name";
-    taskBlock.appendChild(nameLabel);
+
+    const taskName = (label: string, input: string) => {
+      const nameLabel = document.createElement(label) as HTMLElement;
+      nameLabel.id = "nameLabel";
+      nameLabel.setAttribute("for", "name");
+      nameLabel.textContent = "Name:";
+      taskBlock.appendChild(nameLabel);
+      const nameInput = document.createElement(input) as HTMLElement;
+      nameInput.id = "name";
+      taskBlock.appendChild(nameInput);
+    };
+
+    const taskDescription = (label: string, input: string) => {
+      const descriptionLabel = document.createElement(label) as HTMLElement;
+      descriptionLabel.id = "descriptionLabel";
+      descriptionLabel.setAttribute("for", "description");
+      descriptionLabel.textContent = "Description:";
+      taskBlock.appendChild(descriptionLabel);
+      const descriptionInput = document.createElement(input) as HTMLElement;
+      descriptionInput.id = "description";
+      taskBlock.appendChild(descriptionInput);
+    };
+
+    return {
+      taskName,
+      taskDescription,
+    };
   };
 
   const addTasks = () => {
     removeElements("none");
   };
 
+  const createTaskForm = () => {
+    const displayTask = displayTaskAdd()
+    displayTask.taskName("label", "input")
+    displayTask.taskDescription("label", "input")
+  }
+
+
   addTask.addEventListener("click", () => {
     addTasks();
-    displayTaskAdd("label");
+    createTaskForm()
   });
 };
 
