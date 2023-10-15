c468260a541100471633ab3e0641333ebd3b01b4
mjmealey <markjmealey@gmail.com>
2023-10-14 19:56:45 -0500

    name label locate test complete

--- to-do-list/tests/playwright/src/todo/taskBlock.spec.ts
+++ to-do-list/tests/playwright/src/todo/taskBlock.spec.ts
@@ -4,9 +4,11 @@ test.beforeEach(async ({ page }) => {
   const link: string = "http://localhost:5173/";
   await page.goto(link);
   await page.locator("#addTask").click();
+  const taskBlock = "#taskBlock";
+  page.locator(taskBlock);
 });
 
-test("append taskBlock to toDos", async ({page}) => {
-    const taskBlock = "#taskBlock"
-     page.locator(taskBlock)
-})
+test("locate name label", async ({ page }) => {
+  const nameLabel: string = "#nameLabel"
+  page.locator(nameLabel)
+});
